75cf87a355fe584cc601d9fa45ace4e23b8adbf3
Dave Marr <dave.marr@gmail.com>
2014-01-19 16:43:24 -0800

    ex1.{3,4,5}.{scm|txt}

--- ch1/ex1.3.scm
+++ ch1/ex1.3.scm
@@ -1,10 +1,9 @@
-(define (square x) (* x x))
+(define (sum-of-squares x y)
+  (+ (* x x) (* y y)))
 
-(define (test a b c)
-    (if (< a b)
-        (if (< a c)
-            (+ (square b) (square c))
-            (+ (square a) (square b)))
-        (if (< b c)
-            (+ (square a) (square c))
-            (+ (square a) (square b)))))
+(define (larger x y x)
+  (cond
+   ((and (> x y) (> y z)) (sum-of-squares x y))
+   ((and (> x y) (> z y)) (sum-of-squares x z))
+   (else (sum-of-squares y z))))
+  
